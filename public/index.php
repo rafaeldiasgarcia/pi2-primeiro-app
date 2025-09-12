3a4e35911444c8e271cf055417fb292ee7e8f7c7
Rafael Dias Garcia <138343340+rafaeldiasgarcia@users.noreply.github.com>
2025-09-12 00:00:35 +0000

    Aula 11/09/2025

--- public/index.php
+++ public/index.php
@@ -1,17 +1,55 @@
-<?php
-require '../vendor/autoload.php';
-
-use Application\Hello;
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <link href="/css/default.css" rel="stylesheet" />
-    <title>Document</title>
-</head>
-<body>
-    <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/css/default.css" rel="stylesheet" />
+        <title>Larika</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-..." crossorigin="anonymous">
+        <link href="./css/default.css" rel="stylesheet" />
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                     <img src="./img/ham-ico-64.png" alt="" width="32" height="32">
+                    Larika
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain"
+                     aria-controls="navbarMain" aria-expanded="false">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarMain">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home Page</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="menuLinks" role="button" 
+                                 data-bs-toggle="dropdown" aria-expanded="false"> Cardapio </a>
+                            <ul class="dropdown-menu" aria-labelledby="menuLinks">
+                                <li>
+                                    <a class="dropdown-item" href="#">Hamburger</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Hot Dog</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Batatas Recheadas</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Petiscos</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Bebidas</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav> 
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
+    </body>
 </html>
